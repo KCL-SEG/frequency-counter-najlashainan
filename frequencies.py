ee12f64964d51893a960cbaefbb9ce300c2ed6d4
Naj Sh <najsh@Najs-MacBook-Pro.local>
2023-10-09 18:56:53 +0100

    add solution to frequencies.py

--- frequencies.py
+++ frequencies.py
@@ -2,6 +2,16 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def frequencies(items):
-    frequencies = {}
-    # Your code goes here
+    frequency = {}
+   
+    for item in items:
+        string_item = str(item)
+
+        if string_item in frequency:
+            frequency[string_item] += 1
+        else:
+            frequency[string_item] = 1    
     return frequencies
+
+result1 = frequencies(['a', 'a' , 'b', 'b', 'b', 'c'])
+print(result1)
