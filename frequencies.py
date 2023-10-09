39fb4ec1d644ff66de5f13997b0be682c6409d65
Naj Sh <najsh@Najs-MacBook-Pro.local>
2023-10-09 19:06:00 +0100

    add solution to frequencies.py

--- frequencies.py
+++ frequencies.py
@@ -2,16 +2,24 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def frequencies(items):
-    frequency = {}
-   
+    # Create an empty dictionary to store frequencies
+    frequency_dict = {}
+
+    # Iterate through the items list
     for item in items:
-        string_item = str(item)
+        # Convert the item to a string if it's not already
+        item_str = str(item)
 
-        if string_item in frequency:
-            frequency[string_item] += 1
+        # Check if the item_str is already a key in the dictionary
+        if item_str in frequency_dict:
+            # If it is, increment the count
+            frequency_dict[item_str] += 1
         else:
-            frequency[string_item] = 1    
-    return frequencies
+            # If it's not, add it to the dictionary with a count of 1
+            frequency_dict[item_str] = 1
 
-result1 = frequencies(['a', 'a' , 'b', 'b', 'b', 'c'])
-print(result1)
+    return frequency_dict
+
+# Test cases
+result1 = frequencies(['a', 'a', 'b', 'b', 'b', 'c'])
+print(result1)  # Output: { 'a': 2, 'b': 3, 'c': 1 }
