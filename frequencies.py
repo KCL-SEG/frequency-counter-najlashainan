2508ddc56f777e474296209433fedfefec17d7b5
Najla Shainan <najlanassersh@gmail.com>
2023-10-19 21:25:52 +0100

    add frequency counter

--- frequencies.py
+++ frequencies.py
@@ -1,25 +1,16 @@
 """Frequencies function."""
-"""ENTER YOUR SOLUTION HERE!"""
 
 def frequencies(items):
-    # Create an empty dictionary to store frequencies
-    frequency_dict = {}
-
-    # Iterate through the items list
-    for item in items:
-        # Convert the item to a string if it's not already
-        item_str = str(item)
-
-        # Check if the item_str is already a key in the dictionary
-        if item_str in frequency_dict:
-            # If it is, increment the count
-            frequency_dict[item_str] += 1
+    frequencies = {}
+    for i in items:
+        i = str(i) #convert to string
+        if i not in frequencies.keys() :
+            frequencies[i] = 1
         else:
-            # If it's not, add it to the dictionary with a count of 1
-            frequency_dict[item_str] = 1
+            frequencies[i]+= 1
 
-    return frequency_dict
+    return frequencies
 
-# Test cases
+
 result1 = frequencies(['a', 'a', 'b', 'b', 'b', 'c'])
 print(result1)  # Output: { 'a': 2, 'b': 3, 'c': 1 }
